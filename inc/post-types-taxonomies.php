0e6e55676b104954a97040d51e37133720bcd864
EngEng-N <eeng3906@gmail.com>
2025-02-27 14:20:04 -0800

    Registered custom taxonomy for students

--- inc/post-types-taxonomies.php
+++ inc/post-types-taxonomies.php
@@ -72,8 +72,47 @@ function schoolsite_register_custom_post_types() {
 }
 add_action( 'init', 'schoolsite_register_custom_post_types' );
 
+function schoolsite_register_taxonomies() {
+    
+    $labels = array(
+        'name'                  => _x( 'Student Categories', 'taxonomy general name', 'school-theme' ),
+        'singular_name'         => _x( 'Student Category', 'taxonomy singular name', 'school-theme' ),
+        'search_items'          => __( 'Search Student Categories', 'school-theme' ),
+        'all_items'             => __( 'All Student Category', 'school-theme' ),
+        'parent_item'           => __( 'Parent Student Category', 'school-theme' ),
+        'parent_item_colon'     => __( 'Parent Student Category:', 'school-theme' ),
+        'edit_item'             => __( 'Edit Student Category', 'school-theme' ),
+        'view_item'             => __( 'View Student Category', 'school-theme' ),
+        'update_item'           => __( 'Update Student Category', 'school-theme' ),
+        'add_new_item'          => __( 'Add New Student Category', 'school-theme' ),
+        'new_item_name'         => __( 'New Student Category Name', 'school-theme' ),
+        'template_name'         => __( 'Student Category Archives', 'school-theme' ),
+        'menu_name'             => __( 'Student Category', 'school-theme' ),
+        'not_found'             => __( 'No Student categories found.', 'school-theme' ),
+        'no_terms'              => __( 'No Student categories', 'school-theme' ),
+        'items_list_navigation' => __( 'Student Categories list navigation', 'school-theme' ),
+        'items_list'            => __( 'Student Categories list', 'school-theme' ),
+        'item_link'             => __( 'Student Category Link', 'school-theme' ),
+        'item_link_description' => __( 'A link to a student category.', 'school-theme' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'student-categories' ),
+    );
+    register_taxonomy( 'fwd-student-category', array( 'fwd-student' ), $args );
+}
+add_action( 'init', 'schoolsite_register_taxonomies' );
+
 function schoolsite_rewrite_flush() {
     schoolsite_register_custom_post_types();
+    schoolsite_register_taxonomies();
     flush_rewrite_rules();
 }
 add_action( 'after_switch_theme', 'schoolsite_rewrite_flush' );
