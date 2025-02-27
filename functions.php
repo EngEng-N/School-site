eff19cb1a02c3e9987fd798ead63564f61389afb
EngEng-N <eeng3906@gmail.com>
2025-02-26 16:14:19 -0800

    Created CPT for students. Added 2 blocks required for the CPT. Change the "title" placeholder to "Add student name".

--- functions.php
+++ functions.php
@@ -30,3 +30,19 @@ function schoolsite_setup(){
     add_editor_style(get_stylesheet_uri());
 }
 add_action("after_setup_theme", "schoolsite_setup");
+
+// Codes from chatgpt
+// Change the 'title' placeholder when the slug is "students" 
+// This only applies to Students CPT
+function change_student_cpt_title_placeholder($title, $post) {
+    if ($post->post_type == 'fwd-student') {
+        return 'Add student name';
+    }
+    return $title;
+}
+add_filter('enter_title_here', 'change_student_cpt_title_placeholder', 10, 2);
+
+/**
+* Custom Post Types & Custom Taxonomies
+*/
+require get_template_directory() . '/inc/post-types-taxonomies.php';
